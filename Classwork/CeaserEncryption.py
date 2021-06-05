ce03fa14f21a0bfd063df211ec57487c8033f4c2
SimplyClueless <benjaminbristowrocks@yahoo.com>
2021-06-05 16:18:11 +1000

    Fixed changes

--- Classwork/CeaserEncryption.py
+++
@@ -1,7 +0,0 @@
-import string
-
-def salad(word, shift): 
-    shifted = str.maketrans(string.ascii_lowercase, string.ascii_lowercase[shift:] + string.ascii_lowercase[:shift])
-    return word.translate(shifted)
-
-print(salad(input("Enter text to be encrypted: "), 5))
--- Classwork/Python/Ceasar.py
+++ Classwork/Python/Ceasar.py
@@ -1,3 +1,6 @@
+import string
 def salad(word, shift):
-    import string; return word.translate(str.maketrans(string.ascii_lowercase, string.ascii_lowercase[shift:] + string.ascii_lowercase[:shift])).translate(str.maketrans(string.ascii_uppercase, string.ascii_uppercase[shift:] + string.ascii_uppercase[:shift]))
+    lowerCharacterShift = str.maketrans(string.ascii_lowercase, string.ascii_lowercase[shift:] + string.ascii_lowercase[:shift])
+    upperCharacterShift = str.maketrans(string.ascii_uppercase, string.ascii_uppercase[shift:] + string.ascii_uppercase[:shift])
+    return word.translate(lowerCharacterShift).translate(upperCharacterShift)
 print(salad(input("Enter text to be encrypted: "), int(input("Enter amount to shift characters by: "))))
